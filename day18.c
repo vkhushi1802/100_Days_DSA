1bf957427aec6bcea6a787547a59f1d1913f70ce
khushi1802@gmail.com <vkhushi1802@gmail.com>
2026-03-06 23:41:39 +0530

    day18

---
+++ day18.c
@@ -0,0 +1,100 @@
+// Problem: Find Intersection Point of Two Linked Lists - Implement using linked list with dynamic memory allocation.
+
+// Input:
+// - First line: integer n
+// - Second line: n space-separated integers (first list)
+// - Third line: integer m
+// - Fourth line: m space-separated integers (second list)
+
+// Output:
+// - Print value of intersection node or 'No Intersection'
+
+// Example:
+// Input:
+// 5
+// 10 20 30 40 50
+// 4
+// 15 25 30 40 50
+
+// Output:
+// 30
+
+// Explanation:
+// Calculate lengths, advance pointer in longer list, traverse both simultaneously. First common node is intersection.
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void append(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct Node* temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+}
+
+int findIntersection(struct Node* head1, struct Node* head2) {
+    struct Node* p1 = head1;
+
+    while (p1 != NULL) {
+        struct Node* p2 = head2;
+
+        while (p2 != NULL) {
+            if (p1->data == p2->data)
+                return p1->data;
+            p2 = p2->next;
+        }
+
+        p1 = p1->next;
+    }
+
+    return -1;
+}
+
+int main() {
+    int n, m, x;
+    struct Node* head1 = NULL;
+    struct Node* head2 = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        append(&head1, x);
+    }
+
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &x);
+        append(&head2, x);
+    }
+
+    int result = findIntersection(head1, head2);
+
+    if (result == -1)
+        printf("No Intersection");
+    else
+        printf("%d", result);
+
+    return 0;
+}
